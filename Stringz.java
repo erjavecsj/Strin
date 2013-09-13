22f97d4b5712c83dba2b3cf18de13419d2dc075e
apcs <apcs@mvhs-u528ap07.dcsdk12.org>
2013-09-13 08:59:27 -0600

    First of Excersizes

---
+++ Stringz.java
@@ -0,0 +1,86 @@
+public class Stringz {
+	
+	public static void main(String[] args) {
+		String str = new String("A string");
+		String s = "A string";
+
+	// 	if (str.equals(s)) {
+	// 		System.out.println("A");
+	// 	}else {
+	// 		System.out.println("B");
+	// 	}
+	
+
+	// for (int i=0; i< str.length(); i++) {
+	// 	System.out.println(str.charAt(i));
+	// }
+
+		// for (int i = 0 ; i< str.length() - 1; i++) {
+		// System.out.println(str.substring(i, i + 2));
+		// }
+
+		// if (contains("baseball", "ball")) {
+		// 	System.out.println("Yes");
+		// } else{
+		// 	System.out.println("No");
+		// } 
+		// 
+		
+		// count("people", "e");
+
+		// isAbecedarian("abdest");
+		System.out.println(isAbecedarian("Spencer"));
+		
+	}
+
+	public static boolean isAbecedarian(String s) {
+		for (int i = 0; i< s.length()-1; i++) {
+			if (s.charAt(i) >= s.charAt(i+1)) {
+				return false;
+			}
+	
+		}
+
+		return true;
+	}
+
+	// public static int count(String s, String t) {
+	// 	int sum = 0;
+	// 	for (int i = 0; i< s.length()-t.length()+1; i++) {
+	// 		if(s.substring(i, i+t.length()).equals(t)) {
+	// 			sum += 1;
+	// 		}
+	// 	}
+
+	// 	System.out.println(sum);
+	// 	return sum;
+		
+	// }
+
+	// // public static boolean hasAnA(String s) {
+	// // 	for (int i = 0; i< s.length(); i++) {
+	// // 		if (s.substring(i, i+1)) {
+	// // 			charAt(i) == 'A';
+	// // 		}
+
+	// 		return true;
+	// 	}
+	// }
+
+	// public static boolean isPaledrome(String s) {
+	// 	for (int i = 0, int j = s.length(); ; ) {
+			
+	// 	}
+	// }
+
+
+
+	// public static boolean contains(String s, String t) {
+	// 	for (int i = 0; i< s.;length()-t.length()+1; i++) {
+	// 		if (s.substring(i, i+t.length()).equals(t)) {
+	// 			return true;
+	// 		}
+	// 	}
+	// 	return false;
+	// }
+}
